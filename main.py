5d041e05c65bb9e21d7b6dd722e0ac6c61c08c78
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-08-20 11:38:16 +0300

    Moving snake to the right when game starts

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ class Snake:
     def __init__(self):
         pos = cell_number / 2
         self.body = [Vector2(pos - 3, pos), Vector2(pos - 2, pos), Vector2(pos - 1, pos)]
+        self.direction = Vector2(1, 0)
     
     def draw(self):
         for block in self.body:
             block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, (56, 102, 65), block_rect)
 
+    def move(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 class Fruit:
     def __init__(self):
@@ -38,11 +43,15 @@ clock = pygame.time.Clock()
 fruit = Fruit()
 snake = Snake()
 
+pygame.time.set_timer(pygame.USEREVENT, 150)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.USEREVENT:
+            snake.move()
 
     screen.fill((167, 201, 87))
     fruit.draw()
